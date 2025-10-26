32a42ec3445b3bb1752b391e4fe73c6377adff51
saran <saran@SARANSHD>
2025-10-26 16:32:08 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include "Board.hpp"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Snek game");
+	window.setFramerateLimit(10);
+
+	Board game;
+
+	// maybe deal with input queueing later
+
+	while (window.isOpen())
+	{
+		while (const std::optional<sf::Event> event = window.pollEvent())
+		{
+			if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+			{
+				game.handle_input(window, keyPressed->scancode);
+			}
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+		if (!window.hasFocus())
+			game.pause();
+
+		game.update();
+		game.render(window);
+	}
+}
